5d12ff8fdad6af714ce55fae9d6e81466089ce23
TheCodeBite <kamh.1996.hmak@gmail.com>
2019-08-14 20:20:39 -0500

    modulo carrera terminado

--- src/app/carreras/carreras.component.ts
+++ src/app/carreras/carreras.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-carreras',
@@ -6,10 +8,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carreras.component.css']
 })
 export class CarrerasComponent implements OnInit {
+  carreras: any;
+  gurdar = 0;
 
-  constructor() { }
+  carreraForm: FormGroup;
+  constructor(private api: ApiService, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.api.getCarreras().subscribe(response => {
+      this.carreras = response;
+    });
+
+    this.iniciarFormulario();
+
   }
+
+  save_data(formulario: any){
+    if(this.gurdar == 0){
+      /// se crea un nuevo usuario
+      this.api.addCarreras(formulario).subscribe(response => {
+        console.log("AGREGADO")
+        this.ngOnInit();
+      })
+    }else{
+      // se editan cambios
+      console.log("se edito el cambio");
+    }
+  }
+
+  iniciarFormulario() {
+    this.gurdar = 0;
+    this.carreraForm = this.fb.group({
+      id: [''],
+      nombre: [''],
+      nomenclatura: [''],
+      tipo: ['UNI']
+    });
+  }
+
+  editar(formulario: any){
+    this.gurdar = 1;
+    this.carreraForm = this.fb.group({
+      id:[formulario.id],
+      nombre: [formulario.nombre],
+      nomenclatura: [formulario.nomenclatura],
+      tipo: [formulario.tipo]
+    });
+  }
+
 
 }
