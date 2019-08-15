8d1cb8acbeaa24b6a3b8c9a6a19f548e771a1384
david <davyd_d4v1d@hotmail.com>
2019-08-14 21:25:58 -0500

    Se conecto la vista con los metodos de aulas

--- src/app/aulas/aulas.component.ts
+++ src/app/aulas/aulas.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-aulas',
@@ -6,10 +9,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aulas.component.css']
 })
 export class AulasComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  formulario: FormGroup;
+  listaAulas: any;
+  constructor(private api: ApiService, private fb: FormBuilder) { 
+    this.formulario = this.fb.group({
+      nombre: ['', Validators.required]
+    });
   }
 
+  ngOnInit() {
+    this.verAulas();
+  }
+  verAulas(){
+    this.api.verAulas().subscribe(response => {
+      this.listaAulas = response;
+      console.log(this.listaAulas)
+    })
+  }
+  agregar(aula){
+    this.formulario = this.fb.group({
+      nombre: [aula.nombre, Validators.required]
+    });
+    this.api.agregarAulas(form).subscribe(response => {
+      console.log("aula agregado con exito!");
+      Swal.fire({
+        title: 'Agregado con exito!',
+        text: 'La aula se agrego correctamente.',
+        confirmButtonText: 'OK',
+        type: 'success'
+      }).then((restult) => {
+        this.ngOnInit();
+      });
+    })
+  }
+
+  editar(aula) {
+    this.formulario = this.fb.group({
+      id: [aula.id, Validators.required],
+      nombre: [aula.nombre, Validators.required]
+    });
+  }
+
+  guardar(form: any) {
+    this.api.editarMaestro(form.id, form).subscribe(response => {
+      Swal.fire({
+        title: 'Editado con exito!',
+        text: 'Los datos han sido cambiado exitosamente.',
+        confirmButtonText: 'OK',
+        type: 'success'
+      }).then((restult) => {
+        this.ngOnInit();
+      }); 
+    }, err => {
+      console.log("UPS! ");
+      console.log(err.error)
+    })
+  }
 }
